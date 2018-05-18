7e66ecf19087ba3bb4f329f099f435572a36fa48
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-05-17 22:51:32 -0500

    Name fix

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 ./rancher-compose \
---project-name sa-students-ms \
+--project-name runapp \
 --url http://192.168.99.100:8080/v1/projects/1a5 \
---access-key E3DAD90462DC720E5EE3 \
---secret-key YYVEKU7jg1KAJ1V5Rnarq4nNBQxTyzV6UQGM6nTz \
+--access-key 83995506F43A23A88964 \
+--secret-key b6nQKUwj2kVd1dV6vDEgyi7KDjjPERSq8nYNJzC3 \
 -f docker-compose.yml \
 --verbose up \
 -d --force-upgrade \
