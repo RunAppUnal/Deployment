8454e085823e4db6eeaffc8aec75acaac7457bc6
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-05-20 16:25:09 -0500

    add front deployment and scaling

---
+++ back/start.sh
@@ -0,0 +1,9 @@
+.././rancher-compose \
+--project-name runapp-back \
+--url http://192.168.99.100:8080/v1/projects/1a5 \
+--access-key E3DAD90462DC720E5EE3 \
+--secret-key YYVEKU7jg1KAJ1V5Rnarq4nNBQxTyzV6UQGM6nTz \
+-f docker-compose.yml \
+--verbose up \
+-d --force-upgrade \
+--confirm-upgrade 
--- start.sh
+++
@@ -1,9 +0,0 @@
-./rancher-compose \
---project-name runapp \
---url http://192.168.99.101:8080/v1/projects/1a5 \
---access-key 83995506F43A23A88964 \
---secret-key b6nQKUwj2kVd1dV6vDEgyi7KDjjPERSq8nYNJzC3 \
--f docker-compose.yml \
---verbose up \
--d --force-upgrade \
---confirm-upgrade
